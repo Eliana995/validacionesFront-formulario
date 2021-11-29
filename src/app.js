b95ead981898625d586ef22281da63ac1889f84a
Eliana995 <eliana.ag.95@gmail.com>
2021-11-29 14:30:03 -0300

    tp validaciones front de formulario

---
+++ src/app.js
@@ -0,0 +1,67 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
+var logger = require('morgan');
+
+//Ejecuto el llamado a mis rutas
+const indexRouter = require('./routes/index');
+const moviesRoutes = require('./routes/moviesRoutes');
+const genresRoutes = require('./routes/genresRoutes');
+
+//Aquí llamo a la ruta de las api de movies
+const apiMoviesRouter = require('./routes/api/movies')
+//Aquí llamo a la ruta de las api de actors
+const apiGenresRouter = require('./routes/api/genres')
+//Aquí llamo a la ruta de las api de actors
+const apiActorsRouter = require('./routes/api/actors')
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+//Aquí estoy disponiendo la posibilidad para utilizar el seteo en los formularios para el usod e los metodos put ó delete
+app.use(methodOverride('_method'));
+
+app.use('/', indexRouter);
+app.use(moviesRoutes);
+app.use(genresRoutes);
+//Aquí creo la colección de mis recursos de movies (APIs)
+app.use('/api/movies',apiMoviesRouter);
+app.use('/api/actors',apiActorsRouter);
+app.use('/api/genres',apiGenresRouter);
+
+
+//Activando el servidor desde express
+app.listen('3001', () => console.log('Servidor corriendo en el puerto 3001'));
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
